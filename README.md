028b0980b313790ce0611f4071f25ce35dd511f8
Selient <selient@gmail.com>
2020-11-21 00:33:47 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-jsconfig.json
+
 Quickly create a jsconfig.json for node.js development
+
+# jsconfig.json
+
+```
+npx node-jsconfig.json
+```
---
+++ src/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const inquirer = require('inquirer');
+const path = require('path');
+const { writeFile, readdir, readFile } = require('fs').promises;
+
+const configFiles = {};
+const configFolderPath = path.resolve(__dirname, 'config');
+
+(async () => {
+  const files = await readdir(configFolderPath).catch(console.log);
+
+  for (let i of files) {
+    // framework name is situated between 2 dots eg- react between 2 '.'(s)
+    const frameworkName = i.split('.')[1];
+    configFiles[frameworkName] = path.join(configFolderPath, i);
+  }
+
+  const { framework } = await inquirer.prompt([
+    {
+      type: 'list',
+      message: "Pick the framework you're using:",
+      name: 'framework',
+      choices: Object.keys(configFiles),
+    },
+  ]);
+
+  const config = await readFile(configFiles[framework]).catch(console.log);
+
+  const jsconfig = path.join(process.cwd(), 'jsconfig.json');
+
+  await writeFile(jsconfig, JSON.stringify(config.toString(), null, 2));
+
+  console.log('jsconfig.json successfully created');
+})();
